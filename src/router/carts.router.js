58ed015a4e98ec25f7155dee0c956a39e617fc05
Matias Britez <matiasbritez88@gmail.com>
2023-10-27 17:27:15 -0300

    product-cart controllers terminados

--- src/router/carts.router.js
+++ src/router/carts.router.js
@@ -1,6 +1,5 @@
 import Router from "express";
 
-import { cartManager } from "../Dao/MongoDB/cart.js";
 import {
   deleteCart,
   deleteProductByIdFromCart,
@@ -23,17 +22,5 @@ router.delete("/:cid/product/:pid", deleteProductByIdFromCart);
 router.put("/:cid", updateCart);
 
 router.put("/:cid/product/:pid", updateProductByIdFromCartById);
-/*
-router.put("/:cid/product/:pid", async (req, res) => {
-  try {
-    const { cid, pid } = req.params;
 
-    const cart = await cartManager.updateProductFromCart(cid, pid);
-
-    res.status(200).json({ message: "Product edited", cart });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-*/
 export default router;
